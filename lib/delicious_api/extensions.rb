036530ba3b33806c46822d73a53302a3b2892ed8
Javier Blanco <jbgutierrez@gmail.com>
2009-05-02 11:58:52 +0200

    Ruby 1.9 compat: Change the require line to use a full path instead of a relative path

--- lib/delicious_api/extensions.rb
+++ lib/delicious_api/extensions.rb
@@ -1,6 +1,5 @@
 require 'active_support'
 
 Dir[File.dirname(__FILE__) + "/extensions/*.rb"].sort.each do |path|
-  filename = File.basename(path, '.rb')
-  require "extensions/#{filename}"
+  require path
 end
